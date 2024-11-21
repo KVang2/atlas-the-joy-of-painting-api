dd4065874ff3481c6e0b771ce0b18c0961284215
kvang2 <kevinvaj88@hotmail.com>
2024-11-21 09:28:00 -0600

    updating my readme

--- Joy-of-Painting.sql
+++ Joy-of-Painting.sql
@@ -1,19 +1,34 @@
 // Episodes Table
-CREATE TABLE Ep (
+CREATE TABLE episode (
     id UUID PRIMARY KEY,
     title TEXT NOT NULL,
-    ep_numb INT NOT NULL,
-    paint_name TEXT,
-    artist_id UUID REFERENCES Artists(id)
+    episode INT NOT NULL,
+    season INT NOT NULL,
+    broadcast_date DATE NOT NULL
+);
+
+// painting Table
+CREATE TABLE painting (
+    id UUID PRIMARY KEY,
+    painting_index INT NOT NULL,
+    img_src TEXT NOT NULL,
+    title TEXT NOT NULL,
+    num_colors INT NOT NULL,
+    youtube_src TEXT NOT NULL,
+    episode_id UUID REFERENCES episodes(id)
+);
+
+// Subject Table
+CREATE TABLE subject (
+    id UUID PRIMARY KEY,
+    episode INT NOT NULL,
+    title TEXT NOT NULL,
+    obj TEXT NOT NULL
 );
 
 // Colors Table
 CREATE TABLE colors (
     id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    name TEXT NOT NULL,
-    bio TEXT
+    name TEXT NOT NULL UNIQUE,
+    hex_code TEXT NOT NULL
 );
-
-// Sub Table
-CREATE TABLE sub
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# atlas-the-joy-of-painting-api
+# ETL: The Joy of Coding
+
+# Task 0: Design a Database
+- create a design document using UML documentation for the database
+- Create SQL scripts to create database from scratch based on design document
+
+# Task 1: Extract, Transform, Load
+- Write a custom scripts to import data correctly from these data files into your new database
