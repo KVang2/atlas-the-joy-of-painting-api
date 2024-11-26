d4a04ef0e0596512dbfbd16fcc12117f0ad2a2ab
kvang2 <kevinvaj88@hotmail.com>
2024-11-26 12:08:09 -0600

    save transform

---
+++ transform.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const filePaths = ['Episode.csv', 'Colors.csv', 'Subject.csv'];
+
+function transformData(row, sourceFile) {
+    
+}
